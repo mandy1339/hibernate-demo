2edb4da0f44fe674b8802fb3262694f927fa46bf
mandy1339 <mandy1339@gmail.com>
2018-07-17 20:42:02 -0400

    automated git push script

--- src/com/jdbc/TestJdbc.java
+++ src/com/jdbc/TestJdbc.java
@@ -1,10 +1,23 @@
 package com.jdbc;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class TestJdbc {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		String jdbcUrl = "jdbc:mysql://localhost:3306/hb_student_tracker?useSSL=false&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+		String user = "hbstudent";
+		String pass = "hbstudent";
+		try {
+			System.out.println("connecting to database: " + jdbcUrl);
+			Connection myConn = DriverManager.getConnection(jdbcUrl, user, pass);
+			System.out.println("connection successful");
+		}
+		catch (Exception exc) {
+			exc.printStackTrace();
+		}
 	}
 
 }
